10e745ff4c5602a81a60a5831fec47b2a45a7aad
Lukas Grünwald <gruenwald.lk@outlook.com>
2020-06-09 18:40:35 +0200

    Initial Release

---
+++ scripts/library/eaw-log/sinks/file.lua
@@ -0,0 +1,17 @@
+return {
+    __important = true,
+    __target = "eaw-lua.log",
+    __log = function(self, message)
+        _CustomScriptMessage(self.__target, tostring(message))
+    end,
+    with_target_file = function(self, target)
+        if nil == target then
+            return
+        end
+        if type(target) ~= "string" then
+            return
+        end
+        self.__target = target
+        return self
+    end
+}
